9fa41c71e0ddaf0d3e89fc0eca5b3f9009c8be87
Ryan Helm <rjhelm@github.io>
2021-06-22 20:33:37 -0600

    schemas started

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePicId: {
+        type: String
+    },
+    friendList: {
+        type: [Schema.Types.ObjectId],
+        ref: 'User'
+    },
+    powerLevel: {
+        String
+    },
+    decks: {
+        String
+    }
+}
+
+module.exports = mongoose.model('User', userSchema);
