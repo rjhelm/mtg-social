1f5b8d0d7d49c5bfbf136395249ad61ff41ec353
Ryan Helm <rjhelm@github.io>
2021-06-24 20:35:05 -0600

    changed model layout

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,37 +1,142 @@
-const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
-const dateFormat = require('../utils/dateFormat');
+const mongoose = require('mongoose');
+const schemaCleaner = require('../utils/schemaCleaner');
 
-const PostSchema = new Schema(
-  {
-    postText: {
-      type: String,
-      required: 'You need to leave a thought!',
-      minlength: 1,
-      maxlength: 280
+const replySchema = new mongoose.Schema({
+    repliedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: timestamp => dateFormat(timestamp)
+    replyBody: {
+        type: String,
+        trim: true,
     },
-    username: {
-      type: String,
-      required: true
+    upvotedBy: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+    downvotedBy: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+    pointsCount: {
+        type: Number,
+        default: 1,
     },
-    reactions: [CommentSchema]
-  },
-  {
-    toJSON: {
-      getters: true
-    }
-  }
-);
-
-PostSchema.virtual('commentCount').get(function() {
-  return this.comment.length;
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
 });
 
-const Post = model('Post', PostSchema);
+const commentSchema = new mongoose.Schema({
+    commentedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    commentBody: {
+        type: String,
+        trim: true,
+    },
+    replies: [replySchema],
+    upvotedBy: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+    downvotedBy: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+    ],
+    pointsCount: {
+        type: Number,
+        default: 1,
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
 
-module.exports = Post;
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxLength: 100,
+        trim: true,
+    },
+    postType: {
+        type: String,
+        required: true,
+    },
+    textSubmission: {
+        type: String,
+        trim: true,
+    },
+    linkSubmission: {
+        type: String,
+        trim: true,
+    },
+    imageSubmission: {
+        imageLink: {
+            type: String,
+            trim: true,
+        },
+        imageId: {
+            type: String,
+            trim: true,
+        },
+    },
+    section: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Section',
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    upvotedBy: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+    downvotedBy: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+    pointsCount: {
+        type: Number,
+        default: 1,
+    },
+    voteRatio: {
+        type: Number,
+        default: 0,
+    },
+    hotAlgo: {
+        type: Number,
+        default: Date.now,
+    },
+    controversialAlgo: {
+        type: Number,
+        default: 0,
+    },
+    comments: [commentSchema],
+    commentCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
+
+// replace _id with id || create string from objectId || delete __v  //
+schemaCleaner(postSchema);
+schemaCleaner(commentSchema);
+schemaCleaner(replySchema);
+
+module.exports = mongoose.model('Post', postSchema);
