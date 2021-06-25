890aeb52889720e8767d9d8554e6d7cbf98a20ba
Ryan Helm <rjhelm@github.io>
2021-06-24 23:28:49 -0600

    user controller|dependency setup

--- server/utils/config.js
+++ server/utils/config.js
@@ -4,7 +4,7 @@ const cloudinary = require('cloudinary').v2;
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 const SECRET = process.env.SECRET;
-const UPLOAD_PRESET = process.env.UPLOAD_PRESET || 'm1_default';
+const UPLOAD_PRESET = process.env.UPLOAD_PRESET || 'ml_default';
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
