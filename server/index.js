b637f0b386d96952d2e4b4cc833fc58cf94acf4d
rjhelm <rjhelm@github.io>
2021-07-03 23:37:57 -0600

    bug

--- server/index.js
+++ server/index.js
@@ -7,12 +7,7 @@ const resolvers = require('./graphql/resolvers');
 const { PORT } = require('./utils/config');
 const path = require('path')
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+
 
 const app = express();
 
@@ -24,13 +19,21 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req })
 });
 
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../build/index.html')));
   
 }
 
-server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
