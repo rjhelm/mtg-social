1575fa71800220c91113fff00a30d98d7a095bd4
rjhelm <rjhelm@github.io>
2021-07-04 20:49:29 -0600

    another test

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const db = require("./db");
+const cors = require("cors");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { PORT } = require("./utils/config");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: ({ req }) => ({ req }),
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once("open", () => {
+	app.listen(PORT, () => {
+		console.log(`API server running on port ${PORT}!`);
+		console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+	});
+});
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/client/public/index.html"));
+});
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/build/index.html")));
+}
