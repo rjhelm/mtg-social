49a8a9f47bf3087aefc452e3fd8d1180d8380984
rjhelm <rjhelm@github.io>
2021-07-04 22:01:54 -0600

    test

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ db.once("open", () => {
 	});
 });
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, ",,/client/public/index.html"));
+	res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, ",,/client/build/index.html")));
+	app.use(express.static(path.join(__dirname, "../client/build/index.html")));
 }
